53250d624a250f7a2e0ffc7ab68b006b3d99de5d
EC2 Default User <ec2-user@ip-172-31-20-155.us-west-2.compute.internal>
2020-03-19 19:46:48 +0000

    Started add schedule page

--- zlp-scheduler/app/controllers/student_controller.rb
+++ zlp-scheduler/app/controllers/student_controller.rb
@@ -1,4 +1,7 @@
 class StudentController < ApplicationController
   def view_terms
   end
+  
+  def add_schedule
+  end
 end
--- zlp-scheduler/config/routes.rb
+++ zlp-scheduler/config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   root 'login#index'
   
   get 'student/view_terms', to: 'student#view_terms', as: 'view_terms'
+  
+  get 'student/add_schedule', to: 'student#add_schedule', as: 'add_schedule'
 end
