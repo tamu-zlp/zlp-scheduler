bb269103ba5cbeccc0066d3c323096d4131f90fe
EC2 Default User <ec2-user@ip-172-31-20-155.us-west-2.compute.internal>
2020-03-24 03:12:15 +0000

    Add admin pages

---
+++ zlp-scheduler/app/controllers/admin_controller.rb
@@ -0,0 +1,10 @@
+class AdminController < ApplicationController
+  def view_cohorts
+  end
+  
+  def add_cohort
+  end
+  
+  def edit_cohort
+  end
+end
--- zlp-scheduler/config/routes.rb
+++ zlp-scheduler/config/routes.rb
@@ -6,4 +6,12 @@ Rails.application.routes.draw do
   get 'student/view_terms', to: 'student#view_terms', as: 'view_terms'
   
   get 'student/add_schedule', to: 'student#add_schedule', as: 'add_schedule'
+  
+  get 'admin/view_cohorts', to: 'admin#view_cohorts', as: 'view_cohorts'
+  
+  get 'admin/add_cohort', to: 'admin#add_cohort', as: 'add_cohort'
+  
+  get 'admin/edit_cohort', to: 'admin#edit_cohort', as: 'edit_cohort'
+  
+  get 'admin/view_cohort_semester', to: 'admin#view_cohort_semester', as: 'view_cohort_semester'
 end
