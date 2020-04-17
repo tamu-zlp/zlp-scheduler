94c39fefa44c2e3afcfafd8657a9bb1678844afa
Ceryni <matthew.fitzpatrick6012@gmail.com>
2020-04-17 08:25:31 +0000

    Added columns to the terms table as well as a function that can be used to import terms.

--- zlp-scheduler/app/models/term.rb
+++ zlp-scheduler/app/models/term.rb
@@ -1,3 +1,17 @@
 class Term < ApplicationRecord
-    has_many :courses
+  has_many :courses
+
+  def self.RefreshTermList
+    json_terms = CourseScraper.get_terms
+    json_terms.each do |json_term|
+      term = Term.new
+      term.name = json_term['description']
+      term.term_code = json_term['code']
+      begin
+        term.save
+      rescue ActiveRecord::RecordNotUnique
+        Rails.logger.debug "Duplicate Term Detected, Ignoring."
+      end
+    end
+  end
 end
--- zlp-scheduler/db/schema.rb
+++ zlp-scheduler/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200331035806) do
+ActiveRecord::Schema.define(version: 20200417080248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +36,12 @@ ActiveRecord::Schema.define(version: 20200331035806) do
 
   create_table "terms", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
+    t.string   "term_code"
+    t.boolean  "active",                  default: false
+    t.boolean  "courses_import_complete", default: false
+    t.index ["term_code"], name: "index_terms_on_term_code", unique: true, using: :btree
   end
 
   create_table "users", force: :cascade do |t|
