72e48ad2755f376c3af15d60c60d72e702d160ed
EC2 Default User <ec2-user@ip-172-31-20-155.us-west-2.compute.internal>
2020-04-30 22:05:59 +0000

    Checking if term is open for redirect to view terms page

--- zlp-scheduler/app/controllers/sessions_controller.rb
+++ zlp-scheduler/app/controllers/sessions_controller.rb
@@ -6,10 +6,19 @@ class SessionsController < ApplicationController
       @user = User.find_by_email(params[:session][:email].downcase)
       if @user && @user.authenticate(params[:session][:password])
         session[:user_id] = @user.id
+        @term = Term.find_by active: 1
+        if @term.opendate.nil? 
+          @term.update_attributes(:opendate => DateTime.yesterday)
+          @term.update_attributes(:closedate => DateTime.yesterday)
+        end
         if current_user.admin?
           redirect_to view_term_admin_path, :notice => "Logged in !" 
         else
-           redirect_to view_terms_path, :notice => "Logged in !" 
+          if DateTime.current >= @term.opendate && DateTime.current < @term.closedate
+            redirect_to '/student/view_terms', :notice => "Logged in !" 
+          else
+            redirect_to '/student/closed', :notice => "Logged in !" 
+          end
         end
       else
         flash[:login_errors]= ['Email or password is invalid']
