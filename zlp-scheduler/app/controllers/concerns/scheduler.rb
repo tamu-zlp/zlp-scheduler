0836c7be84f431f56bff55f3caed1dc11c1612bb
Parsa Dastjerdi <parsadastjerdi@gmail.com>
2020-05-05 04:49:03 +0000

    up until get_user_schedules completed

--- zlp-scheduler/app/controllers/concerns/scheduler.rb
+++ zlp-scheduler/app/controllers/concerns/scheduler.rb
@@ -7,111 +7,66 @@ class Integer
     MIN = -MAX - 1
 end
 
+'''
+Class: Scheduler
+Takes as input a set of schedules for all students (change this to a cohort maybe?)
+
+'''
 class Scheduler
-    def initialize(schedules)
-        @schedules = schedules
-        # @schedules = convert_to_matrix(schedules)
+    def initialize
+        # users = User.all
+        # @schedules = get_all_schedules(users)
     end
     
     '''
-    # takes all schedules and returns the matrix format for all of them
-    def convert_to_matrix(schedules)
+    # gets all schedules in matrix format for a set of users
+    def get_all_schedules #(users)
+        users = User.all
+        all_schedules = []
         
-        matrix_schedules = Array.new
+        users.each do |user|
+            if user.student?
+                all_schedules.push(get_user_schedules(user.schedules))
+            end
+        end
+        
+        return all_schedules
+    end
+    '''
+    
+    
+    # converts all schedules for a specific student into matrix format
+    def get_user_schedules(schedules)
+        # schedules = User.all[2].schedules
+        user_schedule_list = []
         
         schedules.each do |schedule|
-            
+            user_schedule_list.push(generate_student_schedule(schedule.courses))
         end
         
-        return matrix_schedules
+        return user_schedule_list # matrix of schedules for a single user: [S, S, S]
     end
     
-    '''
     
     # takes as input a list of courses for a student and outputs the schedule in matrix format
-    def generate_schedule # (courses)
-        # remove later
-        user = User.all[3]
-        course = user.schedules[0].courses[0]
-        
-        # initialize schedule 
+    def generate_student_schedule(courses)
+        # courses = User.all[1].schedules[0].courses
         num_days = 5
         num_time_slots = 111
-        schedule = Array.new(num_time_slots, Array.new(num_days, 0))
-        
+        schedule = Array.new(num_time_slots) { Array.new(num_days, 0)} # important !!!!!
+    
         # run for loop for all courses here
-        days = parse_day(course.meeting_days)
-        start_time = parse_time(course.meetingtime_start)
-        end_time = parse_time(course.meetingtime_end)
-        schedule = insert_course(schedule, days, start_time, end_time)
-        
-        puts start_time, end_time, days
+        courses.each do |course|
+            days = parse_day(course.meeting_days)
+            start_time = parse_time(course.meetingtime_start)
+            end_time = parse_time(course.meetingtime_end)
+            schedule = insert_course(schedule, days, start_time, end_time)
+        end
         
         return schedule
     end
     
     
-    # takes DateTime object and returns index corresponding to the time in the schedule
-    def parse_time(time)
-        # puts time
-        zone = ActiveSupport::TimeZone.new("Central Time (US & Canada)")
-        time = time.in_time_zone(zone)
-        hour = (time.hour - 8) * 12
-        minute = time.min / 5
-        return hour + minute # this is the index
-    end
-    
-    
-    def parse_day(days)
-        day_indices = Array.new
-        
-        days.each do |day|
-            if day == 'M'
-                day_indices.push(0)
-            elsif day == 'T'
-                day_indices.push(1)
-            elsif day == 'W'
-                day_indices.push(2)
-            elsif day == 'TR'
-                day_indices.push(3)
-            elsif day == 'F'
-                day_indices.push(4)
-            else 
-                day_indices.push(0) # not sure what to make the default case
-            end
-        end
-        
-        return day_indices
-    end
-
-    
-    # insert all courses into the schedule
-    def insert_course(schedule, days, start_time, end_time)
-        puts "before"
-        puts schedule[0]
-        puts schedule[40]
-        puts schedule[110]
-        
-        days.each do |day|
-            for time in start_time...end_time
-                schedule[time][day] = 1
-            end
-            
-            for time in 0...(start_time - 1)
-                schedule[time][day] = 0
-            end
-        end
-        
-        puts "after"
-        puts schedule[0]
-        puts schedule[40]
-        puts schedule[110]
-        
-        puts schedule.length
-        puts schedule[0].length
-        
-        return schedule
-    end
     
 
     # uses n^m size N-ary string for indexing 
@@ -198,5 +153,54 @@ class Scheduler
         end
 
         return min_index, min
+    end
+    
+    
+    
+    # takes DateTime object and returns index corresponding to the time in the schedule
+    def parse_time(time)
+        # puts time
+        zone = ActiveSupport::TimeZone.new("Central Time (US & Canada)")
+        time = time.in_time_zone(zone)
+        hour = (time.hour - 8) * 12
+        minute = time.min / 5
+        return hour + minute # this is the index
+    end
+    
+    
+    
+    def parse_day(days)
+        day_indices = Array.new
+        
+        days.each do |day|
+            if day == 'M'
+                day_indices.push(0)
+            elsif day == 'T'
+                day_indices.push(1)
+            elsif day == 'W'
+                day_indices.push(2)
+            elsif day == 'TR'
+                day_indices.push(3)
+            elsif day == 'F'
+                day_indices.push(4)
+            else 
+                day_indices.push(0) # not sure what to make the default case
+            end
+        end
+        
+        return day_indices
+    end
+
+    
+    # insert a single course into a schedule
+    def insert_course(schedule, days, start_time, end_time)
+
+        days.each do |day|
+            for time in start_time...end_time
+                schedule[time][day] = 1
+            end
+        end
+        
+        return schedule
     end
 end
