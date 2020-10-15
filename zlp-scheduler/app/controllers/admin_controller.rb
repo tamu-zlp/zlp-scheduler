734a9a69f839677a486a326022b297e8c76604bd
Ubuntu <ubuntu@ip-172-31-6-63.us-east-2.compute.internal>
2020-10-15 04:34:46 +0000

    open term scenarios added

--- zlp-scheduler/app/controllers/admin_controller.rb
+++ zlp-scheduler/app/controllers/admin_controller.rb
@@ -50,7 +50,7 @@ class AdminController < ApplicationController
       @term = Term.find_by active: 1
       open = DateTime.new(params[:term]['opendate(1i)'].to_i, params[:term]['opendate(2i)'].to_i, params[:term]['opendate(3i)'].to_i, params[:term]['opendate(4i)'].to_i, params[:term]['opendate(5i)'].to_i)
       close = DateTime.new(params[:term]['closedate(1i)'].to_i, params[:term]['closedate(2i)'].to_i, params[:term]['closedate(3i)'].to_i, params[:term]['closedate(4i)'].to_i, params[:term]['closedate(5i)'].to_i)
-      LoadCoursesJob.set(wait_until: 2.hours.until.open).perform_later(@term)
+      LoadCoursesJob.perform_later(@term)
       if @term.update_attributes(:opendate => open) && @term.update_attributes(:closedate => close)
         flash[:notice] = 'Term open dates updated.'
         redirect_to view_term_admin_path
--- zlp-scheduler/features/step_definitions/terms_steps.rb
+++ zlp-scheduler/features/step_definitions/terms_steps.rb
@@ -1,3 +1,5 @@
+require 'cucumber/rspec/doubles'
+
 Given /the following terms exist/ do |terms_table|
     terms_table.hashes.each do |term|
         Term.create term
@@ -13,7 +15,13 @@ end
 Given /the active term is "(.*)"/ do |term|
     active_term = Term.find_by(:name => term)
     Term.update_all active: false
-    active_term.update_attributes!(:active => true)
+    active_term.update_attributes!({:active => true, :opendate => DateTime.yesterday, :closedate => DateTime.yesterday, :term_code => "code", :courses_import_complete => true})
+end
+
+Given /"(.*)" is in the current term/ do |cohort|
+    term = Term.find_by(:active => true)
+    active_cohort = Cohort.find_by(:name => cohort)
+    active_cohort.term_id = term.id
 end
 
 Given /I am logged in as an admin/ do
@@ -30,6 +38,18 @@ When /I fill in the new term form/ do
     select("New Test Term", from: 'Term')
     select("Apple", from: 'cohort_select')
     click_button("Activate")
+end
+
+When /I fill in the open term form/ do
+    Time::DATE_FORMATS[:month] = '%B'
+    option = DateTime.current.next_month.to_s(:month)
+    select(option, from: 'term_closedate_2i')
+    if option == 'January'
+        Time::Date_FORMATS[:year] = '%Y'
+        year = DateTime.current.next_month.to_s(:year)
+        select(year, from: 'term_closedate_1i')
+    end
+    click_button("Save")
 end
 
 Then /I should (not )?see "(.*)"/ do |is_not, string|
