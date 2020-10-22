82f9fc8b2a93662c5ba0b62be0d3a30ba5e155fb
Ubuntu <ubuntu@ip-172-31-40-27.us-east-2.compute.internal>
2020-10-22 01:15:42 +0000

    view result connection

--- zlp-scheduler/app/controllers/admin_controller.rb
+++ zlp-scheduler/app/controllers/admin_controller.rb
@@ -1,6 +1,6 @@
 class AdminController < ApplicationController
   
-  before_action :require_admin, only: [:view_term_admin, :open_semester, :new_term, :update_term, :manage_cohorts, :add_cohort, :manage_administrators]    
+  before_action :require_admin, only: [:view_term_admin, :open_semester, :new_term, :update_term, :manage_cohorts, :add_cohort, :manage_administrators, :view_result]    
   
   
   def view_term_admin
@@ -125,4 +125,8 @@ class AdminController < ApplicationController
     @scheduler.optimize
   end
   
+  def view_result
+    @results = TimeSlot.where(:was_conflict => false)
+ 
+  end
 end
