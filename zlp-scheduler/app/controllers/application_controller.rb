fe8ee80891450481d832e73838181f058b1add14
Henry <wanghy917@gmail.com>
2020-11-04 20:55:38 -0600

    merge ui code, fix logo link

--- zlp-scheduler/app/controllers/application_controller.rb
+++ zlp-scheduler/app/controllers/application_controller.rb
@@ -1,21 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-  helper_method :current_user 
+  helper_method :current_user, :logged_in?
 
-    def current_user 
+    def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id] 
-    end 
+    end
     
-    def require_user 
+    def require_user
       redirect_to '/login' unless current_user 
     end
     
     def require_student 
-  		redirect_to '/' unless current_user.student? 
+  		redirect_to '/' unless current_user and current_user.student? 
     end
     
     def require_admin 
-  		redirect_to '/' unless current_user.admin? 
+  		redirect_to '/' unless current_user and current_user.admin? 
+    end
+    def logged_in?
+      !current_user.nil?
     end
 end
